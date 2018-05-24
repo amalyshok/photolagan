ae0cb22275d2a714086989848ce02535bf276379
Aleks M <amalyshok@mail.ru>
2018-05-25 00:22:47 +0300

    Добавлен переход с других страниц на галерею Отображение альбома, на который перешли Автоопределение размера header

---
+++ js/galleryTabs.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	
+	//When page loads...
+	
+	var num = document.location.href.length;
+	var param = document.location.href.substring(num-1);
+	if (param == 'l') param = 0;
+	
+	$("ul.tabs li").removeClass("active"); //Remove any "active" class
+	$(`ul.tabs li:eq(${param})`).addClass("active"); //Add "active" class to selected tab
+	
+		$(".tab_content").hide(); //Hide all tab content
+		let activeTab = $(`ul.tabs li:eq(${param})`).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+		$(activeTab).fadeIn(); //Fade in the active ID content
+		let valueLi = $(activeTab).find('.pagination').find('li').length;
+		$('.gallery_container')[0].style.height = 127 + valueLi*121 + 'px';
+	//On Click Event
+	$("ul.tabs li").click(onClick);
+});
+
+function onClick() {
+		$("ul.tabs li").removeClass("active"); //Remove any "active" class
+		$(this).addClass("active"); //Add "active" class to selected tab
+		$(".tab_content").hide(); //Hide all tab content	
+		var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+		$(activeTab).fadeIn(); //Fade in the active ID content
+		let valueLi = $(activeTab).find('.pagination').find('li').length;
+		$('.gallery_container')[0].style.height = 127 + valueLi*121 + 'px';
+		return false;
+	}
+
+	
--- js/tabs.js
+++ js/tabs.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 	
 	//On Click Event
 	$("ul.tabs li").click(function() {
-	
 		$("ul.tabs li").removeClass("active"); //Remove any "active" class
 		$(this).addClass("active"); //Add "active" class to selected tab
 		$(".tab_content").hide(); //Hide all tab content
@@ -18,3 +17,8 @@ $(document).ready(function() {
 	});
 	
 });
+
+function gotoGallery (num) {
+	document.location = `gallery.html?id=${num}`;
+}
+
